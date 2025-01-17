827ef7aad6cb58ec763feb5735366c1c04225ddf
Daniel Ng <dani3l.yng@gmail.com>
2025-01-16 23:30:10 -0800

    started day 2 exercise

---
+++ day_2/variable.py
@@ -0,0 +1,43 @@
+# Day 2: 30 Days of python programming
+
+# Declare a first name variable and assign a value to it
+
+first_name = 'John'
+
+# Declare a last name variable and assign a value to it
+
+last_name = 'Doe'
+
+# Declare a full name variable and assign a value to it
+
+full_name = 'John Doe'
+
+# Declare a country variable and assign a value to it
+
+country = 'United States'
+
+# Declare a city variable and assign a value to it
+
+city = 'Portland'
+
+# Declare an age variable and assign a value to it
+
+age = 25
+
+# Declare a year variable and assign a value to it
+
+year = 2000
+
+# Declare a variable is_married and assign a value to it
+
+is_married = 'No'
+
+# Declare a variable is_true and assign a value to it
+
+is_true = 'Yes'
+
+# Declare a variable is_light_on and assign a value to it
+
+is_light_on = 'Yes'
+
+# Declare multiple variable on one line
