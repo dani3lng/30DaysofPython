dd08f24e1b4625a23bfca54796b2c06a82bb9647
Daniel Ng <dani3l.yng@gmail.com>
2025-04-18 03:37:30 -0700

    wokring on exercise day 3

---
+++ 03_Day/exercise.py
@@ -0,0 +1,15 @@
+# Exercise: Day 3
+# Declare your age as integer variable
+age = int(32)
+print(age)
+# Declare your height as a float variable
+height = float(175.26)
+print(height)
+# Declare a variable that store a complex number
+compl_num = 1 + 1j
+print(compl_num)
+# Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+base = int(input('Enter the base: '))
+height = int(input('Enter the height: '))
+area = 0.5 * base * height
+print(area)
