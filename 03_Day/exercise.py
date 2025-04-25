79d3e80a97aa0063091f545be519cf758f4c0818
Daniel Ng <dani3l.yng@gmail.com>
2025-04-25 07:03:38 -0700

    compeleted more tasks

--- 03_Day/exercise.py
+++ 03_Day/exercise.py
@@ -41,26 +41,55 @@ slope_eight = (-2 - 0)/(0 - 1)
 print(f"The slope is {slope_eight}")
 # Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
 slope_nine = (10 - 2)/(6 - 2)
-print(f"The slope for (2, 2) and (6, 10) is {slope_nine})")
+print(f"The slope for (2, 2) and (6, 10) is {slope_nine}")
 # Compare the slopes in tasks 8 and 9.
 print(f"The slope of task 8 is {slope_eight} and the slope of task 9 is {slope_nine}")
 # Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
-x = 0
-y = x^2 + (6*x) + 9
-while y != 0:
-    x = 0
-    y = x^2 + (6*x) + 9
-    x += 1
-print(f"Y is equal to zero when x is {x}")
+for x in range(10):
+    y = (x - 3) * (x - 3)
+    if y == 0:
+        print(f"y is zero when x is {x}")
+    else:
+        print(f"{x}")
 # Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+python_len = len('python')
+dragon_len = len('dragon')
+print(python_len == dragon_len)
 # Use and operator to check if 'on' is found in both 'python' and 'dragon'
+if 'on' in 'python' and 'dragon':
+    print('On is in the word python and dragon')
+else:
+    print('On is not in the word python and dragon')
 # I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+if 'jargon' in 'I hope this course is not full of jargon.':
+    print('Jargon is in the sentence.')
+else:
+    print('Jargon is not in the sentence')
 # There is no 'on' in both dragon and python
 # Find the length of the text python and convert the value to float and convert it to string
-# Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+python_len = float(len('python'))
+print(python_len)
+python_len = str(python_len)
+print(python_len)
+# Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python? Use the modulus operator
 # Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+y = 7 % 3
+if y == 2.7: print('True') 
+else: print('False')
 # Check if type of '10' is equal to type of 10
+if type(10) == type('10'): print('True')
+else: print('False')
 # Check if int('9.8') is equal to 10
+if int(9.8) == 10: print('True')
+else: print('False')
 # Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+hours = float(input('How many hours did you work today? '))
+rate_hr = float(input('What is your rate per hour? '))
+pay = (hours * rate_hr)
+print(f"Your pay for today is ${pay:,.2f}")
 # Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+age = int(input('Enter your age: '))
+left = 100 - age
+sec = left * 365 * 24 * 60 
+print(f"A person at the age of {age} has {sec} seconds left before reaching 100 years old.")
 # Write a Python script that displays the following table
